ff034e42f775e67ac2e22cdfc1761b4fc8a6c845
Daniel Pérez <dpdaniperezsanz@gmail.com>
2023-02-28 18:38:23 +0000

    added functions about the type of employee and manager's info

--- starter/index.js
+++ starter/index.js
@@ -12,4 +12,44 @@ const render = require("./src/page-template.js");
 
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
+function managerInformation() {
+    return inquirer.prompt([
+      {
+        type: "input",
+        message: "what is your manager's name?",
+        name: "name"
+      },
+      {
+        type: "input",
+        message: "What is your manager's ID?",
+        name: "id",
+      },
+      {
+        type: "input",
+        message: "What is your manager's email?",
+        name: "email"
+      },
+      {
+        type: "input",
+        message: "What is your manager's office number?",
+        name: "number",
+      },
+    ])
+  };
 
+function typeEmployee() {
+    inquirer.prompt([
+     {
+       type: "list",
+       message: "What type of employee would you like to input",
+       name: "name",
+       choices: ["Engineer", "Intern"],
+     },
+   ]).then(val => {
+      if (val.name === "Engineer") {
+       engineerInformation()
+     } else if (val.name === "Intern") {
+       internInformation();
+     };
+   }); 
+ };
